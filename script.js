04fe5564a544281fb1ec4380b943d71c5239254a
xeniya0410 <kseniatazelova@gmail.com>
2025-09-10 21:22:19 +0500

    script.js

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 let hero = {
-    name: prompt("Введите имя героя:"),
-    age: Number(prompt("Введите ваш возраст:")),
-    hasSwordLicense: prompt("Есть ли у вас лицензия на меч?(true/false):") === "true",
-    hasArmor: prompt("Есть ли у вас броня? (true/false):") === "true",
-    powerLevel: Number(prompt("Введите уровень вашей силы (1-100):")),
+  name: prompt("Введите имя героя: "),
+  age: Number(prompt("Введите ваш возраст: ")),
+  hasSwordLicense: prompt("Есть ли у вас лицензия на меч? (true/false): ") === "true",
+  hasArmor: prompt("Есть ли у вас броня? (true/false): ") === "true",
+  powerLevel: Number(prompt("Введите уровень вашей силы (1-100): ")),
 
-    canFight: function () {
-        return this.age >= 18 && this.hasSwordLicense && this.powerLevel >= 30;
-    },
-    getBattleResult: function () {
-        if (!this.canFight()) {
-            return this.name + ", вы слишком слабы ине можете учавствовать в бою!"
-        }
-        if (this.powerLevel > 70) {
-            return "Поздравляем! Вы победили дракона!";
-        } else {
-            return ("К сожалению, Вы проиграли...");
-        }
-    },
-    calculatePoints: function () {
-        let points = this.powerLevel;
-        if (this.hasSwordLicense) points += 20;
-        if (this.hasArmor) points += 20;
-        return points;
-    },
-    getTitle: function () {
-        let points = this.calculatePoints();
-        if (points >= 140) return "Легендарный герой";
-        if (points >= 100) return "Могучий воин";
-        if (points >= 140) return "Опытный боец";
-        return "Новичок";
+  canFight: function () {
+    return this.age >= 18 && this.hasSwordLicense && this.powerLevel >= 30;
+  },
+
+  getBattleResult: function () {
+    if (!this.canFight()) {
+      return this.name + ", вы слишком слабы и не можете участвовать в бою!";
     }
+    if (this.powerLevel > 70) {
+      return "Поздравляем! Вы победили дракона!";
+    } else {
+      return "К сожалению, вы проиграли...";
+    }
+  },
+
+  calculatePoints: function () {
+    let points = this.powerLevel;
+    if (this.hasSwordLicense) points += 20;
+    if (this.hasArmor) points += 20;
+    return points;
+  },
+
+  getTitle: function () {
+    let points = this.calculatePoints();
+    if (points >= 140) return "Легендарный герой";
+    if (points >= 100) return "Могучий воин";
+    if (points >= 70) return "Опытный боец";
+    return "Новичок";
+  }
 };
 
 if (hero.canFight()) {
-    alert(hero.name + ", вы допущены к бою с драконом!");
-    alert(hero.getBattleResult());
+  console.log(hero.name + ", вы допущены к бою с драконом!");
+  console.log(hero.getBattleResult());
 } else {
-    alert(hero.getBattleResult())
+  console.log(hero.getBattleResult());
 }
 
 let heroPoints = hero.calculatePoints();
-alert("Ваши очки героя:" + heroPoints);
-alert("Ваш титул:" + hero.getTitle());
+console.log("Ваши очки героя: " + heroPoints);
+console.log("Ваш титул: " + hero.getTitle());
